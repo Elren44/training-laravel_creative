e70c59e1b3a25cc4a085ee075c8311f83afd49ff
Sergey Rogozin <danger601@gmail.com>
2023-07-18 10:01:00 +0300

    db update and delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,37 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\View\View;
 
 class PostController extends Controller
 {
     //
 
-    public function index():void {
+    public function index(): void
+    {
 //       $posts = Post::all()->where("is_published", 0)->first();
-       $posts = Post::all()->where("is_published", 1)->first();
+        $posts = Post::all()->where("is_published", 1)->first();
 
-       foreach ($posts->attributesToArray() as $post) {
-           dump($post);
-       }
+        foreach ($posts->attributesToArray() as $post) {
+            dump($post);
+        }
 //       dump($posts->title);
 //       dump("likes: $post->likes");
     }
-    public function create() {
+
+    public function create()
+    {
         $postsArr = [
             [
-                'title'=> 'first post from phpstorm',
-                'content'=> 'first content from created post',
-                'image'=> 'firstsomeimage.jpg',
-                'likes'=> 11,
-                'is_published'=> 1,
+                'title' => 'first post from phpstorm',
+                'content' => 'first content from created post',
+                'image' => 'firstsomeimage.jpg',
+                'likes' => 11,
+                'is_published' => 1,
             ],
             [
-                'title'=> 'second post from phpstorm',
-                'content'=> 'second content from created post',
-                'image'=> 'secondsomeimage.jpg',
-                'likes'=> 22,
-                'is_published'=> 1,
+                'title' => 'second post from phpstorm',
+                'content' => 'second content from created post',
+                'image' => 'secondsomeimage.jpg',
+                'likes' => 22,
+                'is_published' => 1,
             ]
         ];
 //        Post::create([
@@ -47,5 +49,62 @@ class PostController extends Controller
             Post::create($item);
         }
         dd('created');
+    }
+
+    public function update()
+    {
+        $post = Post::find(5);
+
+        $post->update([
+            'title' => 'updated',
+            'content' => 'updated',
+            'image' => 'updated.jpg',
+            'likes' => 22,
+            'is_published' => 1,
+        ]);
+        dd('updated');
+    }
+
+    public function delete()
+    {
+        $post = Post::withTrashed()->find(2);
+        $post->restore();
+        dd('deleted');
+    }
+
+    public function firstOrCreate()
+    {
+        $anotherPost = [
+            'title' => 'some',
+            'content' => 'some',
+            'image' => 'some.jpg',
+            'likes' => 2200,
+            'is_published' => 1,
+        ];
+        $post = Post::firstOrCreate(
+            ['title' => 'some'],
+            [
+                'title' => 'some',
+                'content' => 'some',
+                'image' => 'some.jpg',
+                'likes' => 2200,
+                'is_published' => 1,
+            ]
+        );
+        dd($post->content);
+    }
+
+    public function updateOrCreate() {
+        Post::updateOrCreate(
+            ['title' => 'some2'],
+            [
+                'title' => 'some2',
+                'content' => 'some2',
+                'image' => 'some2.jpg',
+                'likes' => 2200,
+                'is_published' => 1,
+            ]
+        );
+        dd('update_or_create');
     }
 }
